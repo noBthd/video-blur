13ab7ab0c38d21c16c4eb04300c0bc31be728b02
noBthd <Egorkir0401@gmail.com>
2024-10-29 14:41:20 +0300

    some changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
-#include <fstream>
+#include <stdio.h>
 #include <opencv2/highgui.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/core.hpp>
 
+#define VIDEO_INPUT_FOLDER "/Users/egorkirichenko/Projects/C++/video-blur/vid/input"
+#define VIDEO_OUTPUT_FOLDER "/Users/egorkirichenko/Projects/C++/video-blur/vid/output"
 
 int main() {
-    cv::Mat img = cv::imread("/Users/egorkirichenko/Projects/C++/video-blur/img/IMG_0428.JPG", cv::IMREAD_ANYDEPTH);
-
-    cv::imshow("window", img);
+    
 
     cv::waitKey(0);
-
     return 0;
 }
